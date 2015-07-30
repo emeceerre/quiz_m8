df972283102b0f66fe9e7be672ca60c1999e0d78
indi <indi_happy@yahoo.com>
2015-07-30 10:12:42 +0200

    Inicio

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+router.get('/quizes/author', quizController.author);
+
+//Definicion de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+module.exports = router;
